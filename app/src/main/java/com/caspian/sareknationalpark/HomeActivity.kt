02679d4865cc8357db9f81a9e708acb894b21a49
Caspian <87585994+FischerOden@users.noreply.github.com>
2022-05-03 10:18:27 +0200

    expansion

--- app/src/main/java/com/caspian/sareknationalpark/HomeActivity.kt
+++ app/src/main/java/com/caspian/sareknationalpark/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 
 const val EXTRA_MESSAGE = "com.caspian.sareknationalpark.MESSAGE"
+const val EXTRA_IMAGE_SOURCE = "com.caspian.sareknationalpark.IMAGE_SOURCE"
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +14,52 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
     }
 
-    fun textTest(view: View) {
-        val articleTextContent = getString(R.string.bear_text)
-        val intent = Intent(this, TestActivity::class.java).apply {
+    fun textBear(view: View) {
+        var imageSource = "bear"
+        var articleTextContent = getString(R.string.bear_text)
+        var intent = Intent(this, TestActivity::class.java).apply {
             putExtra(EXTRA_MESSAGE, articleTextContent)
+            putExtra(EXTRA_IMAGE_SOURCE, imageSource)
+        }
+        startActivity(intent)
+    }
+
+    fun textGeo(view: View) {
+        var imageSource = "sareklandscape"
+        var articleTextContent = getString(R.string.geo_text)
+        var intent = Intent(this, TestActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, articleTextContent)
+            putExtra(EXTRA_IMAGE_SOURCE, imageSource)
+        }
+        startActivity(intent)
+    }
+
+    fun textGreen(view: View) {
+        var imageSource = "greenerylapland"
+        var articleTextContent = getString(R.string.greenery_text)
+        var intent = Intent(this, TestActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, articleTextContent)
+            putExtra(EXTRA_IMAGE_SOURCE, imageSource)
+        }
+        startActivity(intent)
+    }
+
+    fun textNature(view: View) {
+        var imageSource = "children"
+        var articleTextContent = getString(R.string.nature_text)
+        var intent = Intent(this, TestActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, articleTextContent)
+            putExtra(EXTRA_IMAGE_SOURCE, imageSource)
+        }
+        startActivity(intent)
+    }
+
+    fun textSapmi(view: View) {
+        var imageSource = "flag"
+        var articleTextContent = getString(R.string.sapmi_text)
+        var intent = Intent(this, TestActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, articleTextContent)
+            putExtra(EXTRA_IMAGE_SOURCE, imageSource)
         }
         startActivity(intent)
     }
--- app/src/main/java/com/caspian/sareknationalpark/TestActivity.kt
+++ app/src/main/java/com/caspian/sareknationalpark/TestActivity.kt
@@ -2,6 +2,7 @@ package com.caspian.sareknationalpark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 
 class TestActivity : AppCompatActivity() {
@@ -9,10 +10,27 @@ class TestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
-        val articleTextContent = intent.getStringExtra(EXTRA_MESSAGE)
+        var articleTextContent = intent.getStringExtra(EXTRA_MESSAGE)
+        var imageSource = intent.getStringExtra(EXTRA_IMAGE_SOURCE)
 
-        val textView = findViewById<TextView>(R.id.textView2).apply {
+        var textView = findViewById<TextView>(R.id.textView2).apply {
             text = articleTextContent
+        }
+
+        var imageView = findViewById<ImageView>(R.id.imageView2).apply {
+            if (imageSource == "bear") {
+                setImageResource(R.drawable.bear)
+            } else if (imageSource == "sareklandscape") {
+                setImageResource(R.drawable.sareklandscape)
+            } else if (imageSource == "greenerylapland") {
+                setImageResource(R.drawable.greenerylapland)
+            } else if (imageSource == "children") {
+                setImageResource(R.drawable.children)
+            } else if (imageSource == "flag") {
+                setImageResource(R.drawable.flag)
+            } else {
+                layoutParams.height = 0
+            }
         }
     }
 }
